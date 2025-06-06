c5c030556813d3011ec549949b251e5c23a9324b
sh1xzyy <alekslungol@gmail.com>
2025-06-06 15:14:36 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -46,5 +46,5 @@ export const setupServer = () => {
     });
   });
 
-  app.listen(PORT, () => console.log(`Server is running on port {PORT}`));
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
