5559e1d12e8ce1d359f8f8105b6dc5af56debb77
sh1xzyy <alekslungol@gmail.com>
2025-06-06 15:35:42 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -11,9 +11,12 @@ export const setupServer = () => {
   app.use(cors());
   app.use(express.json());
 
+  app.get('/', (req, res) => {
+    res.send('API is running. Try /contacts');
+  });
+
   app.get('/contacts', async (req, res) => {
     const data = await getContacts();
-    console.log('My Data', data);
 
     res.json({
       status: 200,
@@ -40,10 +43,7 @@ export const setupServer = () => {
   });
 
   app.use((err, req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-      error: err.message,
-    });
+    res.status(404).json({ message: 'Not found' });
   });
 
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
