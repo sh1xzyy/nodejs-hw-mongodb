76ac2ffe80f58cb6b68801076ff6bac43db7f0b8
sh1xzyy <alekslungol@gmail.com>
2025-07-10 23:28:24 +0300

    .

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-import { resolve } from 'node:path';
+import path, { resolve } from 'node:path';
 
 export const sortList = ['asc', 'desc'];
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -6,3 +6,4 @@ export const accessTokenLifeTime = 1000 * 60 * 15;
 export const refreshTokenLifeTime = 1000 * 60 * 60 * 24 * 7;
 export const TEMP_DIR = resolve('temp');
 export const UPLOADS_DIR = resolve('uploads');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { TEMP_DIR, UPLOADS_DIR } from './constants/constants.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { setupServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
